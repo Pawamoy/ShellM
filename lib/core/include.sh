afe7ff4898cefe3ec4c2513a8ea9bc0aaf3a6b66
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-03-09 23:09:47 +0100

    Update include doc, add filter_host function, include now filters files

--- lib/core/include.sh
+++ lib/core/include.sh
@@ -1,41 +1,52 @@
 ## @file include.sh
 ## @brief Include functions for scripts (ndef, define, include)
 
-## @fn int define (name, value)
-## @brief Defines an environment variable in the current shell
-## @param $1 Variable name
-## @param [$2] Variable content (default: 'def')
-## @return Code: 1 if no args or error while affectation, 0 otherwise
-define() {
-	[ $# -ge 1 ] && eval $1=${2:-def} || return 1;
+## @fn filter_host (file, [host])
+## @param file Path to file to filter
+## @param host Optional, name of the host to filter, default to $HOSTNAME
+## @echo Filtered file
+filter_host() {
+  { grep -nE '##.*[\@]host.*[ ,  ]'${2:-$HOSTNAME}'[ ,  ]?' "$1"
+    grep -nv '##.*[\@]host' "$1"
+  } | sort -g | cut -d':' -f2-
 }
 
-## @fn bool ndef (varname)
+## @fn define (name, [value])
+## @brief Defines an environment variable in the current shell
+## @param name Variable name
+## @param value Optional, variable content (default: 'def')
+## @return false if no args or error while affectation, true otherwise
+define() {
+  [ $# -ge 1 ] && eval $1=${2:-def} || return 1;
+}
+
+## @fn ndef (varname)
 ## @brief Tests if a variable is set (non-empty)
-## @param $1 Variable name
+## @param varname Variable name
 ## @return true if empty (unset), false otherwise
 ndef() {
-	[[ $# -ge 1 && -z "$(eval echo \$$1)" ]] || return 1;
+  [[ $# -ge 1 && -z "$(eval echo \$$1)" ]] || return 1;
 }
 
-## @fn int include (filename)
+## @fn include (filename)
 ## @brief Includes content of a library file in the current shell
-## @param $1 Names of library files to include
-## @return Code: 1 (and exits if subshell) if no args or error while including contents, 0 otherwise
-## @return Echo: message on stderr if return code 1
+## @param filename Name of library file to include
+## @echo Message on stderr if return code 1
+## @return false (and exits if subshell) if no args or error while including contents, true otherwise
 include() {
   local libdir
-	local libdirarray
-	IFS=':' read -r -a libdirarray <<< "${LIBPATH}"
-	for libdir in "${libdirarray[@]}"; do
-  	if [ -f "${libdir}/$1" ]; then
-      . "${libdir}/$1" && break
-  		[ $# -ge 1 ] && echo "include: error while including $1 from $0" >&2;
+  local libdirarray
+  IFS=':' read -r -a libdirarray <<< "${LIBPATH}"
+  for libdir in "${libdirarray[@]}"; do
+    if [ -f "${libdir}/$1" ]; then
+      . <(filter_host "${libdir}/$1") && break
+      [ $# -ge 1 ] && echo "include: error while including $1 from $0" >&2;
       [ "$0" != "bash" ] && exit 1 || return 1;
     fi
   done
 }
 
+export -f filter_host
 export -f define
 export -f ndef
 export -f include

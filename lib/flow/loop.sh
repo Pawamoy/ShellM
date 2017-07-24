eb300a63c1f40f54d1f02f2f0320f0e72c120429
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-07-24 16:27:47 +0200

    Update man pages

--- lib/flow/loop.sh
+++ lib/flow/loop.sh
@@ -16,6 +16,36 @@ define  __FLOW_LOOP_SH "
 include core/init/data.sh
 
 ## \brief Control the loops within your scripts (pause/stop them).
+## \desc The loop.sh library provides functions to control loops within shells scripts.
+## When used, it allows to control the execution of already running loops from whatever
+## location or terminal. It is therefore possible to pause a script from another terminal
+## without hitting Control-Z in the shell process running the script.
+##
+## Here is an example of usage in a script called "my-script":
+##
+##     #!/bin/bash
+##     include flow/loop.sh
+##
+##     loop init "my-loop"
+##
+##     i=0
+##     while true; do
+##
+##       loop control "my-loop"
+##
+##       echo "$i"
+##       (( i++ ))
+##       sleep 1
+##     done
+##
+## And the commands used in another shell to control the execution of "my-loop":
+##
+##     $ loop pause "my-script" "my-loop"
+##     $ loop resume "my-script" "my-loop"
+##     $ loop stop "my-script" "my-loop"
+##
+## This execution control mechanism can even allow to control several loops and inner loops
+## (nested loops) at the same time, or make different scripts dependents from each other.
 
 loop_alive() {
   ! loop_dead "$1" "$2"
@@ -48,7 +78,7 @@ loop_init() {
 
 loop_pause() {
   if loop_exists "$1" "$2"; then
-      echo "paused" > "$(get_data_dir loop)/$1_$2"
+    echo "paused" > "$(get_data_dir loop)/$1_$2"
   else
     return 1
   fi

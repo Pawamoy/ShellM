77abc518f63e7696082e41119d53c465928b2723
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-07-02 20:01:34 +0200

    Update shellman location and deps

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 # BINS="bin/shellm-run:bin/shellm-test"
 # LIBS="lib/project.sh"
-# DEPS=shellm/shellm-shellman  #:user2/repo2
+# DEPS=gitlab.com/shellm/shellman  #:user2/repo2
 # BASH_COMPLETIONS="cmp/shellm-project.completion.bash"
 # ZSH_COMPLETIONS="cmp/shellm-project.completion.zsh"
